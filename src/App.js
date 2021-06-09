ab588d1b2a76551dc14021738b25b1e650819786
António Fialho <aluno21895@ipt.pt>
2021-06-09 12:38:01 +0100

    Ligação do React ao Visual Studio

--- src/App.js
+++ src/App.js
@@ -3,14 +3,83 @@
 //****************************/
 
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//import compentes 
+import Tabela from './Tabela'
+
+/**
+ * Função que irá ler os dados da API
+ */
+async function getFotos(){
+  //ler os dados da api
+  let recivefotos = await fetch("/api/FotografiaAPI");
+  
+  if(!recivefotos.ok){
+    //não foi recebido o código 200 do HTTP
+    console.error("Não foi possível ler os dados da API. Código: " + recivefotos.status);
+  }
+  return await recivefotos.json();
+}
 
 /**
  * Componente principal do meu projeto
  */
 class App extends React.Component{
 
+  /**
+   * Construtor da classe -> tem sempre este nome
+   * @param {} props 
+   */
+  constructor(props){
+    super(props); // <--- esta é SEMPRE a primeira instrução
+
+    this.state ={
+      /**
+       * array que irá conter os dados das Fotos, vindas da API
+       */
+      fotos: []
+    }
+  }
+
+  /**
+   * Quando o objeto é criado, executa o código aqui escrito
+   * Vamos usar este método para carregar os dados da API
+   */
+  componentDidMount(){
+    this.loadFotos();
+  }
+
+  /**
+   * Carrega as fotos da API e adiciona-as ao array 'fotos'
+   */
+  async loadFotos(){
+    /**
+     * Taferas:
+     * - 1. Ler os dados da API (fetch)
+     * - 2. atualizar os dados na var. state
+     */
+    try {
+      // 1.
+      let fotosAPI = await getFotos();
+
+      //2.
+      this.setState({
+        fotos:fotosAPI
+      });
+    }catch (erro){ 
+      console.error("Erro na leitura dos dados da API", erro);
+    }
+  }
+
   render(){
-    return (<div></div>);
+    const{fotos}=this.state;
+    return (
+    <div className="container">
+      {/*este componente - Tabela - irá apresentar os dados das 'fotos' no ecrã 
+         as 'fotos' devem ser lidas na API */}
+      <Tabela dadosFotos={fotos} />
+    </div>);
   }
 
 }
---
+++ src/Tabela.js
@@ -0,0 +1,62 @@
+//Tabela.js
+//********************************
+
+
+import React from 'react'
+
+//função que devolve o Cabeçalho da Tabela
+function CabecalhoTabela() {
+    return (
+        <thead>
+            <tr>
+                <th>Nome do Cão</th>
+                <th>Fotografia</th>
+                <th>Local</th>
+                <th>Data</th>
+            </tr>
+        </thead>
+    )
+}
+
+//definição da função que devolve o Corpo da tabela
+//faz exatamente o mesmo da linha 7
+const CorpoTabela = (props) => {
+    //esta função 'interna' irá ler e processar todos
+    //os objetos definidos dentro do array 'dados recebidos'
+    const rows=props.dadosDasFotos.map((row) => {
+        return (
+          <tr key={row.idFoto}>
+              <td>{row.nomeCao}</td>
+              <td><image src={'fotos/' + row.nomeFoto}
+                   alt={'foto do ' + row.nomeCao}
+                   height="50"/></td>
+              <td>{row.localFoto}</td>
+              <td>{row.dataFoto}</td>
+          </tr>  
+        )
+    })
+    //valor devolvido pela função 'CorpoTabela'
+    return (
+        <tbody>{rows}</tbody>
+    )
+}
+
+//componente que junta os dois sub-componentes
+//formando um novo 'componente'
+class Tabela extends React.Component {
+       render() {
+        //estamos a ler os dados que são recebidos pelo componente   
+        // <=> this.props.fotos
+        const{dadosFotos}= this.props
+        return (
+            <table className="table table-striped table-success">
+                <CabecalhoTabela />
+                {/* o parâmetro 'dadosFotos' irá receber os dados que vêm da componente 'mãe'*/}
+                <CorpoTabela  dadosDasFotos={dadosFotos} />
+            </table>
+        )
+    }
+}
+
+export default Tabela
+
